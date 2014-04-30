dacc64004a690703d72b5bc4c71634a2752df2cc
Kevin Shi <kevin@dare2tee.com>
2014-04-30 10:55:06 -0400

    updated complete file

--- server.rb
+++ server.rb
@@ -3,4 +3,15 @@ require "em-websocket"
 EventMachine.run do
 	@channel = EM:Channel.new
 
-	
+	EventMachine::WebSocket.start(host: "0.0.0.0", port: 8080, debug: true) do |ws|
+		ws.onopen do
+			sid = @channel.subscribe { |msg| ws.send(msg) }
+			@channel.push {"#{sid} connected"}
+
+			ws.onmssage { |msg| @channel.push ("<#{sid}> #{msg}") }
+
+			ws.onclose { @channel.unsubscribe(sid) }
+
+		end
+	end
+end
